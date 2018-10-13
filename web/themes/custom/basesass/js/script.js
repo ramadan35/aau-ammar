22f892628769b0d6667bd50359b2f4dc2563c097
aqasem <aqasem@dot.jo>
2018-10-13 15:46:55 +0300

    styling Dynamic header

--- web/themes/custom/basesass/js/script.js
+++ web/themes/custom/basesass/js/script.js
@@ -28,6 +28,19 @@ jQuery(document).ready(function ($) {
 
 
     });
+    
+    var animation = $('.slick__slide .slider_conent').attr('data-animation');
+    $('.slick__slide .text1 , .slick__slide .text2 , .slick__slide .link').removeClass('animated ' + animation);
+
+    // On edge hit
+    $(document).on('afterChange', '#slick-views-dynamic-header-block-1-1', function (event, slick, direction) {
+        var animation = $('.slick-active .slider_conent').attr('data-animation');
+        
+        $('.slick__slide .text1 , .slick__slide .text2 , .slick__slide .link').removeClass('animated ' + animation);
+        $('.slick-active .text1 , .slick-active .text2 , .slick-active .link').addClass('animated ' + animation);
+
+
+    });
 
 
     //parallax
