ac58632dd3f21da1fb8c407dffd1addd76908d8c
aqasem <aqasem@dot.jo>
2018-10-13 15:52:20 +0300

    styling Dynamic header

--- web/themes/custom/basesass/js/script.js
+++ web/themes/custom/basesass/js/script.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function ($) {
     });
 
 
-    var animation = $('.slick__slide .slider_conent').attr('data-animation');
+    var animation = $('.slick-active .slider_conent').attr('data-animation');
     $('.slick__slide .text1 , .slick__slide .text2 , .slick__slide .link').removeClass('animated ' + animation);
     $('.slick-active .text1 , .slick-active .text2 , .slick-active .link').addClass('animated ' + animation);
     // On edge hit
