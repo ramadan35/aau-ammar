858de07cfa9ed1ef89534f78a55eede38e58b9a2
lhmeid <lhmeid@dot.jo>
2018-10-20 21:38:41 +0700

    update

--- web/themes/custom/basesass/js/script.js
+++ web/themes/custom/basesass/js/script.js
@@ -93,7 +93,7 @@ jQuery(document).ready(function ($) {
         slideWidth: 0,
         minSlides: 1,
         responsive: true,
-        controls: true
+        controls: false
     });
     $('#bx-pager').bxSlider({
         mode: 'vertical',
