f95e833d3b23147531401d79b643479750a48911
aqasem <aqasem@dot.jo>
2018-10-21 18:52:55 +0300

    adding new js file

---
+++ web/themes/custom/basesass/js/script1.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+jQuery(document).ready(function ($) {
+
+    $('.page-node-type-media-gallery .bxslider').bxSlider({
+        pagerCustom: '#bx-pager',
+        slideWidth: 0,
+        minSlides: 1,
+        responsive: true,
+        controls: false
+    });
+
+});
